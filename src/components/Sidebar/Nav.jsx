54709fce9c1387c17d231cdf18f67c214ed80fd4
AkiraC0de <ravenakira22@gmail.com>
2025-07-18 14:37:06 +0800

    Added scalable nav bar with config file

--- src/components/Sidebar/Nav.jsx
+++ src/components/Sidebar/Nav.jsx
@@ -1,7 +1,32 @@
+import { NAV_ITEMS } from "../../configs/Nav.config"
+import { useState } from "react"
+
 const Nav = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
   return (
     <nav>
-        
+      <ul className="flex flex-col gap-4">
+        {NAV_ITEMS.map((item, i) => (
+          <li 
+            key={item.id} 
+            className="flex items-center gap-2.5 group cursor-pointer"
+            onClick={() => setActiveIndex(i)}
+          >
+            {<item.icon 
+              className={`${activeIndex == i ? 'bg-primary-red stroke-white' : 'bg-[#f5f8f7] stroke-gray-text group-hover:stroke-black-text' } 
+              rounded-md p-1.5 w-10 h-10 transition-all duration-500 `}
+            />}
+            <h2 
+              className={`${activeIndex == i ? 'text-black-text' : 'text-gray-text'}
+              font-medium group-hover:text-black-text`
+            }>
+              {item.label}
+            </h2>
+          </li>
+          
+        ))}
+      </ul>
     </nav>
   )
 }
--- src/configs/Nav.config.js
+++ src/configs/Nav.config.js
@@ -1,5 +1,8 @@
-import { House } from 'lucide-react';
+import { House,Trophy, Globe, Clapperboard  } from 'lucide-react';
 
 export const NAV_ITEMS = [
-    { label: Home, icon: House, path: '/'},
+    { id: 'nav_home', label: "Home", icon: House, path: '/'},
+    { id: 'nav_global', label: "Global", icon: Globe, path: '/global-news'},
+    { id: 'nav_sports', label: "Sports", icon: Trophy, path: '/sports-news'},
+    { id: 'nav_entertain', label: "Entertainment", icon: Clapperboard, path: '/entertianment-news'},
 ]
